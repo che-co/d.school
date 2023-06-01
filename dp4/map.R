549aefd781b0c83b6bc9c18914af7d4967aa473d
Checo <checo@stanford.edu>
2023-05-31 18:23:31 -0700

    penultimate final

--- dp4/map.R
+++ dp4/map.R
@@ -5,15 +5,18 @@ library(sf)
 
 f_jitter = .2
 a_jitter = 2
-alpha = .2
+alpha = .4
 
-foo = "
 print('reading ira.shp and computing centroids')
-ira = read_sf('ira.shp') %>%
+tmp = "
+read_sf('ira.shp') %>%
 	filter(!SF %in% c('Alaska', 'Hawaii')) %>%
 	st_centroid() %>%
-	st_transform(crs=st_crs('EPSG:5070'))
-"
+	st_transform(crs=st_crs('EPSG:5070')) %>%
+	write_sf('ira_centroid.shp')
+
+ira = read_sf('ira_centroid.shp')
+
 print('jittering 1/7')
 ira_wrk = ira %>%
 	filter(!is.na(D_WKFC_89)) %>%
@@ -51,7 +54,7 @@ ira_trn = ira %>%
 
 ira_t = ira %>%
 	filter(!is.na(D_T))
-
+"
 plt = ggplot() + 
 	theme_void() +
 	geom_sf(data=ira_wrk, aes(size=D_WKFC_89, color='#7f7776'), alpha=alpha) +
@@ -63,10 +66,11 @@ plt = ggplot() +
 	geom_sf(data=ira_t, aes(size=D_T, color='#8c1515'), alpha=alpha) +
 	geom_sf(data=ira_wtr, aes(size=D_WTR_EOMI, color='#007c92'), alpha=alpha) +
 	scale_size(
-		name="Per-capita\nAllocation", 
+		name="Future Spending\nPer Capita", 
 		breaks=c(0, 5e6, 1e7, 1.5e7, 2e7, 2.5e7),
 		labels=c('< $0.5m', '$0.5m', '$1m', '$1.5m', '$2m', '$2.5m'),
-		range=c(.4, 4),
+		range=c(1, 10),
+		guide = guide_legend(reverse=TRUE),
 	) +
 	scale_color_identity(
 		name = 'Disadvantaged\nCategory',
@@ -74,11 +78,20 @@ plt = ggplot() +
 		breaks = c('#7f7776', "#8f993e", "#fedd5c", "#e98300", "#e04f39", "#651c32", "#8c1515", "#007c92"),
 		labels = c('Workforce', "Housing", "Energy", "Pollution", "Transportation", "Climate Change", "Tribal Area", "Water"),
 	) +
-	labs(title = 'The Future of Spending on Enviormental Justice:\nthe Justice 40 Commitment') +
+	labs(
+		title = 'The Justice40 Initiative',
+		subtitle = 'How 40% of the Infaltion Reduction Act will address Enviormental Justice',
+	) +
 	theme(
-		plot.title = element_text(family='monospace', size=12, hjust=.5),
-		legend.title = element_text(family='monospace', size=10),
-		legend.text = element_text(family='monospace'),
-	)
-plt
-ggsave('j40_spending.png')
+		plot.title = element_text(
+			family='monospace', face='bold', size=24, hjust=.5
+		),
+		plot.subtitle = element_text(
+			family='monospace', size=18, hjust=.5
+		),
+		plot.caption = element_text(
+			family='monospace', hjust=.5
+		),
+		legend.title = element_text(family='monospace', size=16),
+		legend.text = element_text(family='monospace', size=14),
+	) 
